8b189f097f014c39df369b6d7c3bc50a9acca875
returnaction <obergannv@gmail.com>
2024-03-02 02:46:32 -0500

    Add Category Index action

---
+++ NikitaBlog/Controllers/CategoryController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NikitaBlog.Models;
+using NikitaBlog.Repository.IRepository;
+
+namespace NikitaBlog.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CategoryController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            List<Category> categories = _unitOfWork.Category.GetAll().ToList();
+            return View(categories);
+        }
+
+        
+    }
+}
