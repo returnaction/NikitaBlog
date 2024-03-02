bb420c3b28f1761292655f754b49fcf811ffbee9
returnaction <obergannv@gmail.com>
2024-03-02 01:38:29 -0500

    Add project files.

---
+++ NikitaBlog/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NikitaBlog.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(25)]
+        public string Title { get; set; }
+
+        public int DisplayOrder { get; set; }
+
+    }
+}
