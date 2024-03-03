7f4dca77392207f778583b5568f0cf0c7b5f8524
returnaction <obergannv@gmail.com>
2024-03-02 22:22:54 -0500

    Add Post Model

---
+++ NikitaBlog/Models/Post.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NikitaBlog.Models
+{
+    public class Post
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Title { get; set; } = null!;
+
+        [Required]
+        [MaxLength(50)]
+        public string Author { get; set; } = null!;
+
+        [Required]
+        [MaxLength(5000)]
+        public string Text { get; set; } = null!;
+
+        [ForeignKey("CategoryId")]
+        [ValidateNever]
+        public Category Category { get; set; }
+        public int CategoryId { get; set; }
+
+        public string? ImageUrl { get; set; }
+
+
+
+    }
+}
