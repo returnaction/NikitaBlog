da226de94867c009641b16fba0e607717a8970e2
returnaction <obergannv@gmail.com>
2024-03-02 23:16:49 -0500

    Add Post Index Action

---
+++ NikitaBlog/Controllers/PostController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using NikitaBlog.Data;
+using NikitaBlog.Models;
+using NikitaBlog.Repository.IRepository;
+
+namespace NikitaBlog.Controllers
+{
+    public class PostController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        
+
+        public PostController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+            
+        }
+
+        public IActionResult Index()
+        {
+            List<Post> posts = _unitOfWork.Post.GetAll().ToList();
+            
+            return View(posts);
+        }
+    }
+}
---
+++ NikitaBlog/Models/PostVM.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace NikitaBlog.Models
+{
+    public class PostVM
+    {
+        public Post Post { get; set; }
+
+        [ValidateNever]
+        public IEnumerable<SelectListItem> CategoryList { get; set; }
+    }
+}
